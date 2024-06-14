a0d3fd14686ede659028052e063df2b93910c2b3
Víctor Fernández <vicbolt.madrid@gmail.com>
2024-06-14 22:37:21 +0200

    cambio en global añadiendo API

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,3 +1,3 @@
 export var Global = {
-    url: 'back-psico-ro2ota0gt-victor-fernandezs-projects-5866bbd1.vercel.app',
+    url: 'back-psico-ro2ota0gt-victor-fernandezs-projects-5866bbd1.vercel.app/api',
 }
