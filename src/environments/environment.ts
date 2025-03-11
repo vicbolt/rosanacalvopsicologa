14c8f4f1b12f4dccea4072753ba16790b2c5ca93
vicbolt <vicbolt.madrid@gmail.com>
2025-03-11 10:33:16 +0100

    environment cambiado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api',  // Usualmente usado en desarrollo
+    apiUrl: 'http://localhost:3000',  // Usualmente usado en desarrollo
   };
